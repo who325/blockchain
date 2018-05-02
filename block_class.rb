21e48e329ddba21c75814fdd5fc5b0da378fc2bc
Nani Kim <appletreehill.hci@gmail.com>
2018-05-02 22:46:01 +0900

    made wallet!

--- block_class.rb
+++ block_class.rb
@@ -1,23 +1,105 @@
 # encoding: UTF-8
 #blockchain code를 넣기 시작하는 코드 
 
+require 'securerandom'
+
 class Blockchain
 
 	def initialize
-
+		@chain = []
+		@trans = []
+		@wallet = {} 
 	end
+
+	def show_all_wallet
+		@wallet 
+	end
+
+
+	def make_a_new_wallet
+		address = SecureRandom.uuid.gsub('-','')
+		@wallet[address] = 1000 #test를 위해서 1000코인이 있다고 하자
+		@wallet
+	end
+
+	#센더랑 리시버가 실제 유효한지 체크: 더블스팬딩 막기위해
+	#지갑이 있는 경우에만 전송을 하겠다
+	def make_a_trans(s,r,a)
+		
+		if @wallet[s].nil?
+			"보내는 주소가 잘못되었음."
+		elsif @wallet[r].nil?
+			"받는 주소가 잘못되었음."
+		elsif @wallet[s].to_f < a.to_f
+			"No money..."
+		else
+			#실제 블록체인과 다름. 원래 컨퍼메이션 할 때 반영이 되는부분임
+			#마이닝을 통해서 거래가 박제가 되었을때 거래를 인정을 하지 실제는.
+			#되게 느리지만 decentralized를 유지하기 위해서 이렇게 함 
+			#아래 부분은 시중은행이 하는 방식이지. 중앙화된.
+			@wallet[s] = @wallet[s].to_f - a.to_f
+			@wallet[r] = @wallet[r].to_f + a.to_f
+
+			trans = {
+				"sender" => s,
+				"receiver" => r,
+				"amount" => a
+			}
+			@trans << trans #주의. 골뱅이 트랜스와 그냥 트랜스는 완전 다른것임
+			@trans
+		end
+	end
+
+
 
 	def mining
+		history = []
+		current_time = Time.now.to_f
 		begin
-			nonce = rand(100) #자신의 난스값 알고리즘이 들어가겠지
-		end while nonce != 0
-		nonce
-		#"Found a block"
+			nonce = rand(10000000) #자신의 난스값 알고리즘이 들어가겠지
+			#nonce값을 기반으로 해싱해보자 - (예시) 해쉬값이 '0000'인 입력값을 찾으시오
+			#보통 어떠한 수식을 만족하는 값으라고 하지
+
+			#검증은 되게 쉬운데 거꾸로 찾기는 어려운 이런 문제를 보통 내지
+			history << nonce #히스토리에 난스값을 밀어넣음
+			hashed = Digest::SHA256.hexdigest(nonce.to_s)
+
+		end while hashed[0..3] != '0000'
+
+		
+
+		nonce #루비는 맨 마지막라인의 이 값만 호출해서 찍힘... 그래서 위에 puts해도 안찍혀
+		#그래서 리턴값을 여러개로 하고싶으면 아래와 같이 하자
+		return_array = []
+
+		return_array << Time.now.to_f - current_time
+		return_array << nonce
+		return_array
+
+		block = {
+			"index" => @chain.size + 1, #몇번째 블록
+			"time" => Time.now.to_i, #현재 타임
+			"nonce" => nonce, #위 난스값을 가진 블록
+			"previous_address" => Digest::SHA256.hexdigest(last_block.to_s),
+			"transactions" => @trans #블록에 거래정보 박제됨
+		}
+		@trans = [] #리셋해야지
+		@chain << block 
+		block 
+
 	end
 
-	def transaction
-		"Finalized"
+	def last_block
+		@chain[-1] #마지막 블럭을 얻어올 수 있음
+	end
+
+	def all_blocks
+		@chain #전체 리스트만 찍어주는 함수
 	end
 
 end
+
+#코빗api 활용 - GET https://api.korbit.co.kr/v1/ticker --> 만들어서 본전찾으면 메일보내는 코드 아마존 서버에 돌려놓으면 됨..
+#DAEMON 을 활용하면 컴터 꺼도 계속 코드 돌아감
+#그래픽카드를 활용했더니 병렬적으로 여러개 동시에 돌릴 수 있음 - 어쩌다 그래픽카드 해봤더니 좋아서 그후로 날개돋힌듯...
 
--- server.rb
+++ server.rb
@@ -7,22 +7,69 @@
 # encoding: UTF-8
 
 require 'sinatra'
-require './block'
-
-get'/' do #이게 주소임. 여기 들어가면 텍스트를 찍자 라는 것
-	"This is Block List."
-end
-
-get'/mine' do
-	aBlock.mining.to_s
-end
-
+require './block_class'
 
 
 aBlock = Blockchain.new
 
+get'/' do #이게 주소임. 여기 들어가면 텍스트를 찍자 라는 것
+
+	message = "<center>"
+
+	aBlock.all_blocks.each do |b| #블록을 하나 뽑아서 돌아가는 것 (내 블록 b)
+		message << "번호: " + b['index'].to_s + "<br>" #ruby는 문자열도 이런식으로 밀어넣을수있음
+		message << "시간: " + b['time'].to_s + "<br>"
+		message << "Nonce: " + b['nonce'].to_s + "<br>"
+		message << "앞 주소: " + b['previous_address'].to_s + "<br>"
+		message << "내 주소: " + Digest::SHA256.hexdigest(b.to_s) + "<br>"
+		message << "Transactions: " + b["transactions"].to_s + "<br>"
+		message << "<hr>" #한줄 그어주는 것
+	end
+	message + "</center>" #</> 문자열을 브라우즈에서 띄우는거기때문에 html로 해석이 되는거임
+end
+
+get'/mine' do
+	aBlock.mining.to_s
+	#to_s : to string, to_i : to integer
+end
+
+
+get'/trans' do
+	aBlock.make_a_trans(params["sender"], params["recv"], params["amount"]).to_s
+	
+end
+
+
+get'/new_wallet' do
+	aBlock.make_a_new_wallet.to_s
+end
+
+get'/all_wallet' do
+	aBlock.show_all_wallet.to_s
+end
 
 
 
-#http://localhost:4567/
+'''
 
+http://localhost:4567/
+이런식으로 블록에 거래정보가 계속 기록되고 
+ 송금자 입금자에게 거래트래픽이 왔다갔다 하는건 아님. 제3자에게 거래정보가 계속 저장되는게 이상하지 ㅋㅋ
+지렁이 게임이랑 비슷하다!
+
+etherscan.io도 시간이 나면 수업시간에...
+
+블록을 해킹하면 주소값이 달라져서 새로운 블록으로 인식됨 -> 도태됨
+
+루비에서 아무것도 없는 빈 리스트를 해싱하면 nil 값이 해싱됨.
+
+컨펌은 결국 뒤에 블록이 붙는 것. 6컨펌은 블록이 붙었는데 뒤에 6개가 더 붙어야 아 안전하구나 하는 것
+
+결국 거래소에서도 똑같은 방식으로 동작을 하는 것 - 주소를 만들어서 보내고 ... 
+박제는 마이닝할때 일어남. 난스값을 찾았으면 블록을 만들때
+
+UUID: universally unique id -> 범용고유식별자 
+이걸 만들겠다 라고 하면 절대 겹칠수 없는 아이디라는 것. 만드는데 시간이 오래걸리면 안됨. 지갑만들때. 
+절대 같을 수 없는것이 보장되는 것. 블록체인 소스보면 알수있음. 같은게 나올 수 없음.
+UUID ruby -> secureRandom이라는 라이브러리 이용
+'''
